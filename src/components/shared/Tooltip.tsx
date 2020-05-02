d290bdfb869cfc7faa044d7a05adb2750fb18626
kovanety <anczernek@gmail.com>
2020-05-02 11:58:21 +0200

    Update meta tags

---
+++ src/components/shared/Tooltip.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { BORDER_RADIUS, TEXT_COLOR } from '../../constants'
+
+export const Tooltip = styled.div<{ isVisible: boolean }>`
+  position: absolute;
+  bottom: 140%;
+  left: 50%;
+  padding: 0.5rem;
+  transform: translateX(-50%);
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: ${BORDER_RADIUS};
+  color: ${TEXT_COLOR.WHITE};
+  transition: opacity 0.3s;
+
+  ::after {
+    content: ' ';
+    position: absolute;
+    width: 0;
+    height: 0;
+    top: 100%;
+    left: 50%;
+    transform: translate(-50%);
+    border: 1rem solid;
+    border-color: rgba(0, 0, 0, 0.8) transparent transparent transparent;
+  }
+`
--- src/components/teampage/TeamInfo.tsx
+++ src/components/teampage/TeamInfo.tsx
@@ -5,11 +5,11 @@ import { useLocation } from '@reach/router'
 import {
   FONT_WEIGHT,
   FONT_SIZE,
-  BORDER_RADIUS,
-  TEXT_COLOR,
   SCREEN_SIZE,
   LEADERBOARD_WIDTH,
 } from '../../constants'
+
+import { Tooltip } from '../shared/Tooltip'
 import { Input } from '../shared/Input'
 
 const Container = styled.div`
@@ -55,31 +55,6 @@ const InputWrapper = styled.div`
   position: relative;
 `
 
-const CopiedTooltip = styled.div<{ isVisible: boolean }>`
-  position: absolute;
-  bottom: 140%;
-  left: 50%;
-  padding: 0.5rem;
-  transform: translateX(-50%);
-  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
-  background: rgba(0, 0, 0, 0.8);
-  border-radius: ${BORDER_RADIUS};
-  color: ${TEXT_COLOR.WHITE};
-  transition: opacity 0.3s;
-
-  ::after {
-    content: ' ';
-    position: absolute;
-    width: 0;
-    height: 0;
-    top: 100%;
-    left: 50%;
-    transform: translate(-50%);
-    border: 1rem solid;
-    border-color: rgba(0, 0, 0, 0.8) transparent transparent transparent;
-  }
-`
-
 interface TeamInfoProps {
   team?: string
 }
@@ -107,7 +82,7 @@ export const TeamInfo: FC<TeamInfoProps> = ({ team }) => {
           Too lazy to click? Let your pals click for you:{' '}
         </Label>
         <InputWrapper>
-          <CopiedTooltip isVisible={isCopied}>Copied!</CopiedTooltip>
+          <Tooltip isVisible={isCopied}>Copied!</Tooltip>
           <Input
             id="copyLink"
             value={href}
